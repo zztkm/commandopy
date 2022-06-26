73605b60c4a0c76ef6a88242f1c42d5d7f824295
zztkm <takumi.basket1682@gmail.com>
2022-06-27 02:39:53 +0900

    update: commando document(github pages)

--- commando/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__version__ = "2.0.0"
--- commando/util.py
+++
@@ -1,5 +0,0 @@
-from typing import List
-
-
-def str_to_list(cmd_str: str) -> List[str]:
-    return list(cmd_str.split())
